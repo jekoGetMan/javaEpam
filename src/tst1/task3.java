d2ec1a38a54893f087b8a9b6e3dc3fdbc07f0aed
jekoGetMan <sl1mvsshady@gmail.com>
2018-11-18 00:00:09 +0200

    les3

---
+++ src/tst1/task3.java
@@ -0,0 +1,13 @@
+package tst1;
+
+public class task3 {
+    public static void main(String[] args) {
+        String a = "\nМоя строка";
+        String b = "\tМоя строка";
+        String c = "Моя строка";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
